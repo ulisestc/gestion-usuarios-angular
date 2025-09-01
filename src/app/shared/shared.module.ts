07dc18cee95001c08d675e05b1375777eb80ce65
ulisestc <ulisestc27@gmail.com>
2025-08-31 22:20:18 -0600

    Navbar working with routerLink

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 // import { AppRoutingModule } from "src/app/app-routing.module";
 
 
@@ -11,6 +12,7 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     // AppRoutingModule
 ],
   exports: [
